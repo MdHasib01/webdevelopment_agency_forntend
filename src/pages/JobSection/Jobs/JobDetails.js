13867a8d99471e9a59ebfd2c302b7c0c419eac96
rashidulrony <rashidulislam.official1@gmail.com>
2022-01-02 14:38:36 +0600

    update

--- src/pages/JobSection/Jobs/JobDetails.js
+++ src/pages/JobSection/Jobs/JobDetails.js
@@ -1,46 +1,54 @@
-import React from 'react';
-import avatar from '../../../Assets/gigImage/gigImage.jpg'
+import React from "react";
+import { useForm } from "react-hook-form";
+import avatar from "../../../Assets/Ellipse 91.png";
 
-const JobDetails = ({job}) => {
+const JobDetails = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="customContainer">
       <div className="jobDetails">
         <div className="buyerProfile">
           <img className="buyerImg" src={avatar} alt="" />
           <div className="buyerInfo">
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
+            <h4>MD. RASHIDUL ISLAM</h4>
           </div>
         </div>
+
         <div className="job-info">
-          <div className="jobLogo">
-          <img  src={avatar} alt="" />
+          <div className="jobReq">
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
+            <h5>Rashidul islam</h5>
           </div>
-        
-       <div className="jobReq">
-       <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5><h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5><h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5><h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-            <h5>Rashidul islam</h5>
-       </div>
-        
-        
+          <div className="comment-section">
+            <div className="display-comment"></div>
+            <div className="add-comment">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("message", { required: true })} />
+                <input type="submit" />
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
